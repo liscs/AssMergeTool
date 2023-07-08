b774e13bf9367f2e22ad566496cebb0f8456cfa4
dreamZdew <dreamZdew@outlook.com>
2023-07-08 18:48:15 +0800

    添加项目文件。

---
+++ Assmodhelper/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace AssMergeTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddAss1 = new System.Windows.Forms.Button();
+            this.DelAss1 = new System.Windows.Forms.Button();
+            this.AssList1 = new System.Windows.Forms.ListBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.Down1 = new System.Windows.Forms.Button();
+            this.Up1 = new System.Windows.Forms.Button();
+            this.Up2 = new System.Windows.Forms.Button();
+            this.Down2 = new System.Windows.Forms.Button();
+            this.AssList2 = new System.Windows.Forms.ListBox();
+            this.DelAss2 = new System.Windows.Forms.Button();
+            this.AddAss2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AddAss1
+            // 
+            this.AddAss1.Location = new System.Drawing.Point(387, 37);
+            this.AddAss1.Name = "AddAss1";
+            this.AddAss1.Size = new System.Drawing.Size(43, 23);
+            this.AddAss1.TabIndex = 1;
+            this.AddAss1.Text = "+";
+            this.AddAss1.UseVisualStyleBackColor = true;
+            this.AddAss1.Click += new System.EventHandler(this.AddAss_Click);
+            // 
+            // DelAss1
+            // 
+            this.DelAss1.Location = new System.Drawing.Point(387, 66);
+            this.DelAss1.Name = "DelAss1";
+            this.DelAss1.Size = new System.Drawing.Size(43, 23);
+            this.DelAss1.TabIndex = 2;
+            this.DelAss1.Text = "-";
+            this.DelAss1.UseVisualStyleBackColor = true;
+            this.DelAss1.Click += new System.EventHandler(this.DelAss_Click);
+            // 
+            // AssList1
+            // 
+            this.AssList1.AllowDrop = true;
+            this.AssList1.FormattingEnabled = true;
+            this.AssList1.ItemHeight = 12;
+            this.AssList1.Location = new System.Drawing.Point(18, 37);
+            this.AssList1.Name = "AssList1";
+            this.AssList1.Size = new System.Drawing.Size(363, 88);
+            this.AssList1.TabIndex = 3;
+            this.AssList1.SelectedIndexChanged += new System.EventHandler(this.AssList1_SelectedIndexChanged);
+            this.AssList1.DragDrop += new System.Windows.Forms.DragEventHandler(this.AssList1_DragDrop);
+            this.AssList1.DragEnter += new System.Windows.Forms.DragEventHandler(this.AssList_DragEnter);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(145, 214);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(55, 23);
+            this.Save.TabIndex = 8;
+            this.Save.Text = "Do it";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Down1
+            // 
+            this.Down1.Enabled = false;
+            this.Down1.Location = new System.Drawing.Point(387, 124);
+            this.Down1.Name = "Down1";
+            this.Down1.Size = new System.Drawing.Size(43, 23);
+            this.Down1.TabIndex = 9;
+            this.Down1.Text = "↓";
+            this.Down1.UseVisualStyleBackColor = true;
+            this.Down1.Click += new System.EventHandler(this.Down_Click);
+            // 
+            // Up1
+            // 
+            this.Up1.Enabled = false;
+            this.Up1.Location = new System.Drawing.Point(387, 95);
+            this.Up1.Name = "Up1";
+            this.Up1.Size = new System.Drawing.Size(43, 23);
+            this.Up1.TabIndex = 10;
+            this.Up1.Text = "↑";
+            this.Up1.UseVisualStyleBackColor = true;
+            this.Up1.Click += new System.EventHandler(this.Up_Click);
+            // 
+            // Up2
+            // 
+            this.Up2.Enabled = false;
+            this.Up2.Location = new System.Drawing.Point(805, 95);
+            this.Up2.Name = "Up2";
+            this.Up2.Size = new System.Drawing.Size(43, 23);
+            this.Up2.TabIndex = 15;
+            this.Up2.Text = "↑";
+            this.Up2.UseVisualStyleBackColor = true;
+            this.Up2.Click += new System.EventHandler(this.Up_Click);
+            // 
+            // Down2
+            // 
+            this.Down2.Enabled = false;
+            this.Down2.Location = new System.Drawing.Point(805, 124);
+            this.Down2.Name = "Down2";
+            this.Down2.Size = new System.Drawing.Size(43, 23);
+            this.Down2.TabIndex = 14;
+            this.Down2.Text = "↓";
+            this.Down2.UseVisualStyleBackColor = true;
+            this.Down2.Click += new System.EventHandler(this.Down_Click);
+            // 
+            // AssList2
+            // 
+            this.AssList2.AllowDrop = true;
+            this.AssList2.FormattingEnabled = true;
+            this.AssList2.ItemHeight = 12;
+            this.AssList2.Location = new System.Drawing.Point(436, 37);
+            this.AssList2.Name = "AssList2";
+            this.AssList2.Size = new System.Drawing.Size(363, 88);
+            this.AssList2.TabIndex = 13;
+            this.AssList2.SelectedIndexChanged += new System.EventHandler(this.AssList2_SelectedIndexChanged);
+            this.AssList2.DragDrop += new System.Windows.Forms.DragEventHandler(this.AssList2_DragDrop);
+            this.AssList2.DragEnter += new System.Windows.Forms.DragEventHandler(this.AssList_DragEnter);
+            // 
+            // DelAss2
+            // 
+            this.DelAss2.Location = new System.Drawing.Point(805, 66);
+            this.DelAss2.Name = "DelAss2";
+            this.DelAss2.Size = new System.Drawing.Size(43, 23);
+            this.DelAss2.TabIndex = 12;
+            this.DelAss2.Text = "-";
+            this.DelAss2.UseVisualStyleBackColor = true;
+            this.DelAss2.Click += new System.EventHandler(this.DelAss_Click);
+            // 
+            // AddAss2
+            // 
+            this.AddAss2.Location = new System.Drawing.Point(805, 37);
+            this.AddAss2.Name = "AddAss2";
+            this.AddAss2.Size = new System.Drawing.Size(43, 23);
+            this.AddAss2.TabIndex = 11;
+            this.AddAss2.Text = "+";
+            this.AddAss2.UseVisualStyleBackColor = true;
+            this.AddAss2.Click += new System.EventHandler(this.AddAss_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 373);
+            this.Controls.Add(this.Up2);
+            this.Controls.Add(this.Down2);
+            this.Controls.Add(this.AssList2);
+            this.Controls.Add(this.DelAss2);
+            this.Controls.Add(this.AddAss2);
+            this.Controls.Add(this.Up1);
+            this.Controls.Add(this.Down1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AssList1);
+            this.Controls.Add(this.DelAss1);
+            this.Controls.Add(this.AddAss1);
+            this.Name = "Form1";
+            this.Text = "ASS向右合并";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button AddAss1;
+        private System.Windows.Forms.Button DelAss1;
+        private System.Windows.Forms.ListBox AssList1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Down1;
+        private System.Windows.Forms.Button Up1;
+        private System.Windows.Forms.Button Up2;
+        private System.Windows.Forms.Button Down2;
+        private System.Windows.Forms.ListBox AssList2;
+        private System.Windows.Forms.Button DelAss2;
+        private System.Windows.Forms.Button AddAss2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assmodhelper/Form1.cs
@@ -0,0 +1,340 @@
+﻿using Nikse.SubtitleEdit.Core.Common;
+using Nikse.SubtitleEdit.Core.SubtitleFormats;
+using System;
+using System.Collections;
+using System.Drawing.Printing;
+using System.Globalization;
+using System.IO;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AssMergeTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void AddAss_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog
+            {
+                FileName = "Select a text file",
+                Filter = @"Ass files (*.ass)|*.ass",
+                Title = @"Open text file",
+                Multiselect = true
+            };
+
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                Button button = (Button)sender;
+                if (button.Name == "AddAss1")
+                {
+                    foreach (var i in openFileDialog1.FileNames) AssList1.Items.Add(i);
+                }
+                else
+                {
+                    foreach (var i in openFileDialog1.FileNames) AssList2.Items.Add(i);
+                }
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                                $"Details:\n\n{ex.StackTrace}");
+            }
+        }
+
+        private void DelAss_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (AssList1.SelectedIndex != -1 && button.Name == "DelAss1")
+                AssList1.Items.RemoveAt(AssList1.SelectedIndex);
+            else if (AssList2.SelectedIndex != -1 && button.Name == "DelAss2")
+                AssList2.Items.RemoveAt(AssList2.SelectedIndex);
+        }
+
+
+        //合并assList中的ass文件，保存到savename，info信息使用2文件
+        private static void SaveAss(ArrayList assList, string savename)
+        {
+            var info = new ArrayList();
+            var styles = new ArrayList();
+            var Event = new ArrayList();
+            assList.Reverse();
+            foreach (var i in assList)
+            {
+                var file1 = new StreamReader(i.ToString());
+                string line;
+                if (assList.IndexOf(i) == 0)
+                    while ((line = file1.ReadLine()) != "[V4+ Styles]")
+                        info.Add(line);
+
+                file1.ReadLine();
+                while ((line = file1.ReadLine()) != "[Events]")
+                {
+                    if (line == "")
+                        continue;
+                    try
+                    {
+                        if (line != null) styles.Add(line.Insert(line.IndexOf(','), '-' + assList.IndexOf(i).ToString()));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.StackTrace);
+                    }
+                }
+
+                file1.ReadLine();
+                while ((line = file1.ReadLine()) != null)
+                {
+                    if (line == "") continue;
+                    try
+                    {
+                        Event.Add(line.Insert(line.IndexOf(',', 34), '-' + assList.IndexOf(i).ToString()));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.StackTrace);
+                    }
+                }
+                file1.Close();
+            }
+
+            using (var file2 = new StreamWriter(savename, false, Encoding.UTF8))
+            {
+                foreach (var i in info) file2.WriteLine(i);
+                file2.WriteLine("[V4+ Styles]");
+                file2.WriteLine(
+                    "Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding");
+                foreach (var i in styles) file2.WriteLine(i);
+                file2.WriteLine("");
+                file2.WriteLine("[Events]");
+                file2.WriteLine("Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text");
+                foreach (var i in Event) file2.WriteLine(i);
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            var assList = new ArrayList();
+            for (int i = 0; i < AssList1.Items.Count && i < AssList2.Items.Count; i++)
+            {
+                ResampleResolution((string)AssList1.Items[i], (string)AssList2.Items[i]);
+                assList.Add(AssList1.Items[i]);
+                assList.Add(AssList2.Items[i]);
+                SaveAss(assList, (string)AssList2.Items[i]);
+                assList.Clear();
+            }
+            MessageBox.Show(@"success!");
+        }
+
+        //重设leftPath对应路径字幕文件的分辨率为rightPath文件分辨率
+        private void ResampleResolution(string leftPath, string rightPath)
+        {
+            Subtitle sub = Subtitle.Parse(leftPath);
+            (int, int) tupleSourceResolution = getResolution(leftPath);
+            (int, int) tupleTargetResolution = getResolution(rightPath);
+
+            int sourceWidth = tupleSourceResolution.Item1, sourceHeight = tupleSourceResolution.Item2;
+            int targetWidth = tupleTargetResolution.Item1, targetHeight = tupleTargetResolution.Item2;
+            var fixMargins = true;
+            var fixFonts = true;
+            var fixPos = true;
+            var fixDraw = true;
+            var styles = AdvancedSubStationAlpha.GetSsaStylesFromHeader(sub.Header);
+            foreach (var style in styles)
+            {
+                if (fixMargins)
+                {
+                    style.MarginLeft = AssaResampler.Resample(sourceWidth, targetWidth, style.MarginLeft);
+                    style.MarginRight = AssaResampler.Resample(sourceWidth, targetWidth, style.MarginRight);
+                    style.MarginVertical = AssaResampler.Resample(sourceHeight, targetHeight, style.MarginVertical);
+                }
+
+                if (fixFonts)
+                {
+                    style.FontSize = AssaResampler.Resample(sourceHeight, targetHeight, style.FontSize);
+                }
+
+                if (fixFonts || fixDraw)
+                {
+                    style.OutlineWidth = (decimal)AssaResampler.Resample(sourceHeight, targetHeight, (float)style.OutlineWidth);
+                    style.ShadowWidth = (decimal)AssaResampler.Resample(sourceHeight, targetHeight, (float)style.ShadowWidth);
+                    style.Spacing = (decimal)AssaResampler.Resample(sourceWidth, targetWidth, (float)style.Spacing);
+                }
+            }
+
+            sub.Header = AdvancedSubStationAlpha.GetHeaderAndStylesFromAdvancedSubStationAlpha(sub.Header, styles);
+
+            sub.Header = AdvancedSubStationAlpha.AddTagToHeader("PlayResX", "PlayResX: " + targetWidth.ToString(CultureInfo.InvariantCulture), "[Script Info]", sub.Header);
+            sub.Header = AdvancedSubStationAlpha.AddTagToHeader("PlayResY", "PlayResY: " + targetHeight.ToString(CultureInfo.InvariantCulture), "[Script Info]", sub.Header);
+
+            foreach (var p in sub.Paragraphs)
+            {
+                if (fixFonts)
+                {
+                    p.Text = AssaResampler.ResampleOverrideTagsFont(sourceWidth, targetWidth, sourceHeight, targetHeight, p.Text);
+                }
+
+                if (fixPos)
+                {
+                    p.Text = AssaResampler.ResampleOverrideTagsPosition(sourceWidth, targetWidth, sourceHeight, targetHeight, p.Text);
+                }
+
+                if (fixDraw)
+                {
+                    p.Text = AssaResampler.ResampleOverrideTagsDrawing(sourceWidth, targetWidth, sourceHeight, targetHeight, p.Text, null);
+                }
+            }
+            SaveSub(sub, leftPath);
+        }
+
+        private void SaveSub(Subtitle sub, string leftPath)
+        {
+            using (var file = new StreamWriter(leftPath, false, Encoding.UTF8))
+            {
+                file.Write(sub.Header);
+                file.WriteLine("Format: Layer,Start,End,Style,Actor,MarginL,MarginR,MarginV,Effect,Text");
+                foreach (var p in sub.Paragraphs)
+                {
+                    var line = "Dialogue: " + p.Layer.ToString() + "," + p.StartTime.ToHHMMSSFF() + "," + p.EndTime.ToHHMMSSFF() + "," +
+                        p.Extra + "," + p.Actor + "," + p.MarginL + "," + p.MarginR + "," +
+                        p.MarginV + "," + p.Effect + "," + p.Text;
+                    file.WriteLine(line);
+                }
+            }
+
+        }
+
+        private (int, int) getResolution(string path)
+        {
+            int width = 0, height = 0;
+            using (var file = new StreamReader(path))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    int index;
+                    if ((index = line.LastIndexOf("PlayResX:")) != -1)
+                    {
+                        width = int.Parse(line.Substring(index + 9).Trim());
+                    }
+                    if ((index = line.LastIndexOf("PlayResY:")) != -1)
+                    {
+                        height = int.Parse(line.Substring(index + 9).Trim());
+                        break;
+                    }
+                }
+            }
+            return (width, height);
+        }
+
+        private void AssList_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
+        }
+
+
+        private void AssList1_DragDrop(object sender, DragEventArgs e)
+        {
+            var fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var t in fileNames)
+                AssList1.Items.Add(t);
+        }
+        private void AssList2_DragDrop(object sender, DragEventArgs e)
+        {
+            var fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var t in fileNames)
+                AssList2.Items.Add(t);
+        }
+
+        private void AssList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (AssList1.Items.Count == 1)
+                return;
+            if (AssList1.SelectedIndex == 0)
+            {
+                Up1.Enabled = false;
+                Down1.Enabled = true;
+            }
+            else if (AssList1.SelectedIndex == AssList1.Items.Count - 1)
+            {
+                Down1.Enabled = false;
+                Up1.Enabled = true;
+            }
+            else
+            {
+                Down1.Enabled = true;
+                Up1.Enabled = true;
+            }
+        }
+        private void AssList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (AssList2.Items.Count == 1)
+                return;
+            if (AssList2.SelectedIndex == 0)
+            {
+                Up2.Enabled = false;
+                Down2.Enabled = true;
+            }
+            else if (AssList2.SelectedIndex == AssList2.Items.Count - 1)
+            {
+                Down2.Enabled = false;
+                Up2.Enabled = true;
+            }
+            else
+            {
+                Down2.Enabled = true;
+                Up2.Enabled = true;
+            }
+        }
+
+        private void Up_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Name == "Up1")
+            {
+                var tem = AssList1.SelectedItem;
+                AssList1.Items[AssList1.SelectedIndex] = AssList1.Items[AssList1.SelectedIndex - 1];
+                AssList1.Items[AssList1.SelectedIndex - 1] = tem;
+                AssList1.SelectedIndex--;
+            }
+            else
+            {
+                var tem = AssList2.SelectedItem;
+                AssList2.Items[AssList2.SelectedIndex] = AssList2.Items[AssList2.SelectedIndex - 1];
+                AssList2.Items[AssList2.SelectedIndex - 1] = tem;
+                AssList2.SelectedIndex--;
+            }
+        }
+
+        private void Down_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Name == "Up1")
+            {
+                var tem = AssList1.SelectedItem;
+                AssList1.Items[AssList1.SelectedIndex] = AssList1.Items[AssList1.SelectedIndex + 1];
+                AssList1.Items[AssList1.SelectedIndex + 1] = tem;
+                AssList1.SelectedIndex++;
+            }
+            else
+            {
+                var tem = AssList2.SelectedItem;
+                AssList2.Items[AssList2.SelectedIndex] = AssList2.Items[AssList2.SelectedIndex + 1];
+                AssList2.Items[AssList2.SelectedIndex + 1] = tem;
+                AssList2.SelectedIndex++;
+            }
+        }
+
+
+    }
+}
